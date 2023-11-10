3b74bcf55af934e97b9c9238b20cedb9c92bb61f
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-11-10 18:40:36 -0500

    2023-11-10T18-40-36Z

--- BranchPointerPass/BranchPointerPass.cpp
+++ BranchPointerPass/BranchPointerPass.cpp
@@ -1,6 +1,6 @@
 #include "llvm/IR/Function.h"
-#include "llvm/IR/Instructions.h" // Include for BranchInst
 #include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instructions.h" // Include for BranchInst
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -14,61 +14,62 @@ using namespace llvm;
 
 namespace {
 
-    void logBranchInstruction(BranchInst *BI) {
-        if (BI->isConditional()) {
-            // Unique identifier for branch, e.g., using the address of the instruction
-            errs() << "br_" << BI << "\n";
-        }
+void logBranchInstruction(BranchInst *BI) {
+  if (BI->isConditional()) {
+    // Unique identifier for branch, e.g., using the address of the instruction
+    errs() << "br_" << BI << "\n";
+  }
+}
+
+void logFunctionPointer(CallInst *CI) {
+  if (Function *calledFunction = CI->getCalledFunction()) {
+    errs() << "*func_" << calledFunction << "\n";
+  }
+}
+
+void visitor(Function &F) {
+  errs() << "Analyzing function " << F.getName() << '\n';
+
+  for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+    Instruction *Inst = &*I;
+
+    if (BranchInst *BI = dyn_cast<BranchInst>(Inst)) {
+      // errs() << "Found a branch instruction!\n";
+      logBranchInstruction(BI);
     }
 
-    void logFunctionPointer(CallInst *CI) {
-        if (Function *calledFunction = CI->getCalledFunction()) {
-            errs() << "*func_" << calledFunction << "\n";
-        }
+    if (CallInst *CI = dyn_cast<CallInst>(Inst)) {
+      logFunctionPointer(CI);
     }
+  }
+}
 
-    void visitor(Function &F) {
-        errs() << "Analyzing function " << F.getName() << '\n';
+struct BranchPointerPass : PassInfoMixin<BranchPointerPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+    visitor(F);
+    return PreservedAnalyses::all();
+  }
 
-        for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-            Instruction *Inst = &*I;
-
-            if (BranchInst *BI = dyn_cast<BranchInst>(Inst)) {
-                errs() << "Found a branch instruction!\n";
-                logBranchInstruction(BI);
-            }
-
-            if (CallInst *CI = dyn_cast<CallInst>(Inst)) {
-                logFunctionPointer(CI);
-            }
-        }
-    }
-
-    struct BranchPointerPass : PassInfoMixin<BranchPointerPass> {
-        PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-            visitor(F);
-            return PreservedAnalyses::all();
-        }
-
-        static bool isRequired() { return true; }
-    };
+  static bool isRequired() { return true; }
+};
 
 } // namespace
 
 llvm::PassPluginLibraryInfo getBranchPointerPluginInfo() {
-    return {LLVM_PLUGIN_API_VERSION, "BranchPointerPass", LLVM_VERSION_STRING,
-            [](PassBuilder &PB) {
-                PB.registerPipelineParsingCallback(
-                    [](StringRef Name, FunctionPassManager &FPM, ArrayRef<PassBuilder::PipelineElement>) {
-                        if (Name == "branch-pointer-pass") {
-                            FPM.addPass(BranchPointerPass());
-                            return true;
-                        }
-                        return false;
-                    });
-            }};
+  return {LLVM_PLUGIN_API_VERSION, "BranchPointerPass", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "branch-pointer-pass") {
+                    FPM.addPass(BranchPointerPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-    return getBranchPointerPluginInfo();
+  return getBranchPointerPluginInfo();
 }
